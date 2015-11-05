84b8d1ed393cb522f47feeff59316152af2eed14
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2015-11-05 20:28:29 -0200

    OpenMPI now works

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CXX = 				mpic++
-CXXFLAGS = 			-I/usr/local/include -O0 -Wall -g -std=c++11 -fopenmp
+CXXFLAGS = 			-I/usr/local/include -O0 -Wall -g 
 #LIBS =    			-L/usr/local/lib -lpthread -ldl -lm -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video -lopencv_objdetect
-LIBS =    			-L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_imgproc
-SRCS = 				main.cpp mainParallel.cpp
+LIBS =    			-L/usr/local/lib/ -lopencv_core -lopencv_highgui -lopencv_imgproc
+SRCS = 				main.cpp
 HEADERS = 	
 BUILD_DIR =			build
 OBJS = 				main.o
--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <mpi.h>
+#include <string>
 #include <unistd.h>
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -20,13 +22,14 @@ void scalar_convolution(cv::Mat& source_image,
 int main( int argc, char** argv ) {
 
     // mpi auxiliars
-    int rank = 0, number_of_procecess = 1;
-    char *cpu_name;
+    int rank = 0, number_of_processes = 1;
+    MPI_Status status;
 
     // auxiliars
-    Mat source_image; Mat destiny_image;
+    Mat source_image, destiny_image, final_image;
     bool black_and_white = false, visual = false;
     bool dont_save_image = false, opencv_std = false, parallel = false;
+    int sizes[3];
 
     // validade the input
     if (argc < 2) {
@@ -58,6 +61,17 @@ int main( int argc, char** argv ) {
         }
     }
 
+    // load the image and validate
+    string image_path(argv[1]);
+    if (rank == 0) {
+
+        if (!black_and_white) {
+            source_image = imread(image_path, CV_LOAD_IMAGE_COLOR);
+        } else {
+            source_image = imread(image_path, CV_LOAD_IMAGE_GRAYSCALE);
+        }
+    }
+
     if (parallel) {
         
         // start MPI processes and get rank
@@ -65,25 +79,59 @@ int main( int argc, char** argv ) {
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
         // how many procecess we have
-        MPI_Comm_size(MPI_COMM_WORLD, &number_of_procecess);
+        MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 
-        // for debugging purposes, get hostname
-        cpu_name    = (char *)calloc(80,sizeof(char));
-        gethostname(cpu_name,80);
-        printf("hello MPI user: from process = %i on machine=%s, of NCPU=%i processes\n",
-               rank, cpu_name, number_of_procecess);
+        // root process distributes images
+        if (rank == 0 && source_image.data) {
+
+            int size_of_pieces = source_image.rows/number_of_processes;
+            int height = size_of_pieces + KERNEL_SIZE/2;
+
+            for (int i = 1; i < number_of_processes; i++) {
+
+                int y = size_of_pieces*i - KERNEL_SIZE/2;
+
+                // create a rectangle representing the area to 'cut'
+                Rect region_of_interest = Rect(0, y, source_image.cols, height);
+                Mat piece = source_image(region_of_interest);
+
+                // compute size to send
+                sizes[0] = piece.size().height;
+                sizes[1] = piece.size().width;
+                sizes[2] = piece.type();
+                MPI_Send(sizes, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+                // send to other processes
+                MPI_Send(piece.data,
+                    sizes[0]*sizes[1]*piece.channels(),
+                    MPI_UNSIGNED_CHAR,
+                    i,
+                    1,
+                    MPI_COMM_WORLD);
+            }
+
+            source_image = source_image(Rect(0, 0, source_image.cols, height));
+        }
+
+        else {
+
+            // get size and create matrix
+            MPI_Recv(sizes, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            source_image = Mat(sizes[0], sizes[1], sizes[2]);
+
+            MPI_Recv(source_image.data,
+                sizes[0]*sizes[1]*source_image.channels(),
+                MPI_UNSIGNED_CHAR,
+                0,
+                1,
+                MPI_COMM_WORLD,
+                &status);
+
+        }
     }
 
-    // load the image and validate
-    string image_path(argv[1]);
-    if (!black_and_white) {
-        source_image = imread(image_path, CV_LOAD_IMAGE_COLOR);
-    } else {
-        source_image = imread(image_path, CV_LOAD_IMAGE_GRAYSCALE);
-    }
-        
     if (!source_image.data) {
-        cerr << "Abort! Couldn't load " << image_path << "!" << endl;
+        cerr << "Abort! Couldn't load image!" << endl;
         return -1;
     }
 
@@ -100,15 +148,58 @@ int main( int argc, char** argv ) {
         scalar_convolution(source_image, destiny_image, kernel);
     }
 
-    if (visual) {
-        imshow("Original Image", source_image);
-        imshow("Blured Image", destiny_image);
-        waitKey(0);
-    }
+    if (rank == 0) {
 
-    if (!dont_save_image) {
-        string new_file_name = "blured_" + to_string(rank) + image_path;
-        imwrite(new_file_name, destiny_image);
+        final_image = destiny_image(Rect(KERNEL_SIZE/2,
+                KERNEL_SIZE/2,
+                destiny_image.cols - KERNEL_SIZE/2-2,
+                destiny_image.rows - KERNEL_SIZE/2));
+        for (int i = 1; i < number_of_processes; i++) {
+
+            // get result from other friends
+            MPI_Recv(sizes, 3, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+            Mat piece(sizes[0], sizes[1], sizes[2]);
+            MPI_Recv(piece.data,
+                sizes[0]*sizes[1]*source_image.channels(),
+                MPI_UNSIGNED_CHAR,
+                i,
+                3,
+                MPI_COMM_WORLD,
+                &status);
+
+            // cut black borders
+            Rect region_of_interest = Rect(KERNEL_SIZE/2,
+                KERNEL_SIZE/2,
+                piece.cols - KERNEL_SIZE/2-2,
+                piece.rows - KERNEL_SIZE/2);
+            piece = piece(region_of_interest);
+
+            // concatenate
+            vconcat(final_image, piece, final_image);
+        } 
+
+        if (visual) {
+            imshow("Original Image", source_image);
+            imshow("Blured Image", destiny_image);
+            waitKey(0);
+        }
+
+        if (!dont_save_image) {
+            string new_file_name = "blured_" + to_string(rank) + image_path;
+            imwrite(new_file_name, final_image);
+        }
+
+    } else {
+
+        // send result to root
+        MPI_Send(sizes, 3, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(destiny_image.data,
+            sizes[0]*sizes[1]*destiny_image.channels(),
+            MPI_UNSIGNED_CHAR,
+            0,
+            3,
+            MPI_COMM_WORLD);
+
     }
 
     // horraaaaay, finish it up
@@ -121,7 +212,7 @@ void scalar_convolution(cv::Mat& source_image,
     cv::Mat& kernel) {
 
     int half_k = kernel.rows/2;
-    int h = source_image.cols, w = source_image.rows;
+    int w = source_image.cols, h = source_image.rows;
     bool black_and_white = source_image.channels() == 1;
 
     // performs convolution
